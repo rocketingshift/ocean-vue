2ab9c3c39845d56b887b2922c5dc1b9eee27b02f
rocketingshift <nlinhchi2020@gmail.com>
2026-07-10 13:34:52 +0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import { onMounted, onUnmounted } from 'vue'
+import { useAppStore } from '@/stores/useAppStore'
+
+const appStore = useAppStore()
+
+onMounted(() => {
+  appStore.init()
+})
+
+onUnmounted(() => {
+  appStore.destroy()
+})
+</script>
