a3044c515112192985089917b7010e69a554f735
rocketingshift <nlinhchi2020@gmail.com>
2026-07-10 14:02:03 +0700

    Create LayoutDefault.vue

---
+++ src/components/layout/LayoutDefault.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="layout-default">
+    <!-- Fixed header (always on top) -->
+    <Header />
+
+    <!-- Preloader (z-index above everything) -->
+    <Preloader />
+
+    <!-- Introduction screen -->
+    <Introduction />
+
+    <!-- Main page content (scroll + WebGL) -->
+    <main class="main">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import Header       from '@/components/common/Header.vue'
+import Preloader    from '@/components/sections/Preloader.vue'
+import Introduction from '@/components/sections/Introduction.vue'
+</script>
+
+<style scoped>
+.layout-default {
+  overflow: clip;
+  position: relative;
+}
+.main {
+  min-height: 100svh;
+  min-height: calc(var(--svh, 1svh) * 100);
+  position: relative;
+}
+</style>
