fb2c6b421fd16c4f925793c0cafd49c120e1a494
rocketingshift <nlinhchi2020@gmail.com>
2026-07-10 14:03:31 +0700

    Create WebGLCanvas.vue

---
+++ src/components/webgl/WebGLCanvas.vue
@@ -0,0 +1,25 @@
+<template>
+  <canvas
+    ref="canvasRef"
+    class="webgl-canvas"
+    aria-hidden="true"
+  />
+</template>
+
+<script setup>
+import { ref }         from 'vue'
+import { useWebGL }    from '@/composables/useWebGL'
+
+const canvasRef = ref(null)
+useWebGL(canvasRef)
+</script>
+
+<style scoped>
+.webgl-canvas {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: 100%;
+  z-index: var(--z-canvas);
+}
+</style>
